a0ffae98f8845f9bda2b35a66c9a969099199a5f
Donovan Clarke <donovanclarketx@gmail.com>
2017-10-14 00:41:33 -0400

    have profit loss totals, work on showing profit / loss, also delete of trade still wip

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,32 +5,31 @@ class TradesController < ApplicationController
     @buy = CoinBuy.select('*').where('email = ?', current_user.email)
     @sell = CoinSell.select('*').where('email = ?', current_user.email)
 
+    @buyAmount = CoinBuy.select('*').where('email = ?', current_user.email).group('coinID')
+    @sellAmount = CoinSell.select('*').where('email = ?', current_user.email).group('coinID')
+
+    buyTotal = @buyAmount.sum(:price)
+    sellTotal = @sellAmount.sum(:price)
+
+
+    @buyTotal = buyTotal.to_a
+    @sellTotal = sellTotal.to_a
+    # @profitTotal = buyTotal.difference(sellTotal)
   end
 
   def create
     if params[:trades] == 'true'
       @trades = CoinBuy.new(trade_params)
-
-      respond_to do |format|
-        if @trades.save
-          format.html { redirect_to trades_list_url, notice: 'Trade sucessfully saved.' }
-          format.json { render :home, status: :created, location: @coin}
-        else
-          format.html { render :list}
-          format.json { render json: @coin.errors, status: :unprocessable_entity }
-        end
-      end
     else
       @trades = CoinSell.new(trade_params)
-
-      respond_to do |format|
-        if @trades.save
-          format.html { redirect_to trades_list_url, notice: 'Trade sucessfully saved.' }
-          format.json { render :home, status: :created, location: @trades}
-        else
-          format.html { render :list}
-          format.json { render json: @trades.errors, status: :unprocessable_entity }
-        end
+    end
+    respond_to do |format|
+      if @trades.save
+        format.html { redirect_to trades_list_url, notice: 'Trade sucessfully saved.' }
+        format.json { render :home, status: :created, location: @coin}
+      else
+        format.html { render :list}
+        format.json { render json: @coin.errors, status: :unprocessable_entity }
       end
     end
   end
